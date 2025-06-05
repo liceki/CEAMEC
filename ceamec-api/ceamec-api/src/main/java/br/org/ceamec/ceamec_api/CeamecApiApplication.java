c8f147e6534b5ca9467ee1e95bf946b7dcbba5e4
Henrique Leão Paim <0063400@academico.ifmg.edu.br>
2025-06-05 00:34:16 -0300

    add: spring project to make api

---
+++ ceamec-api/ceamec-api/src/main/java/br/org/ceamec/ceamec_api/CeamecApiApplication.java
@@ -0,0 +1,13 @@
+package br.org.ceamec.ceamec_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeamecApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeamecApiApplication.class, args);
+	}
+
+}
