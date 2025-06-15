1fab336eda3be265391ce29569bc3851db59f8ea
henrique <henrique.lp2006@gmail.com>
2025-06-15 13:53:39 -0300

    chore: configura segurança básica da aplicação

--- ceamec/src/main/java/com/ifmg/ceamec/CeamecApplication.java
+++ ceamec/src/main/java/com/ifmg/ceamec/CeamecApplication.java
@@ -2,12 +2,16 @@ package com.ifmg.ceamec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CeamecApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CeamecApplication.class, args);
+
+        new SpringApplicationBuilder(CeamecApplication.class)
+                .headless(false)
+                .run(args);
     }
 
 }
